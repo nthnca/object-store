62fbdb9b4d8f3ee28e7bf26d3f85daabf3eb7d26
Nathan Bullock <nathanbullock@gmail.com>
2018-10-11 01:53:43 +0000

    Continue improving tests

--- integration_test.go
+++ integration_test.go
@@ -37,16 +37,35 @@ func TestFullWorkCycle(t *testing.T) {
 	for i := 0; i < 6; i++ {
 		wg.Add(1)
 		go func(z int) {
+			defer wg.Done()
 			for j := 0; j < 2; j++ {
 				log.Printf("Saving: Object:s%d%d", z, j)
 				os1.Insert(ctx, fmt.Sprintf("key%d%d", z, j),
 					[]byte(fmt.Sprintf("data%d%d", z, j)))
 				log.Printf("Saved: Object:s%d%d", z, j)
 			}
-			wg.Done()
 		}(i)
 	}
 	wg.Wait()
+
+	for i := 0; i < 6; i++ {
+		for j := 0; j < 2; j++ {
+			wg.Add(1)
+			go func(x, y int) {
+				defer wg.Done()
+				g := os1.Get(fmt.Sprintf("key%d%d", x, y))
+				if bytes.Compare(g, []byte(fmt.Sprintf("data%d%d", x, y))) != 0 {
+					log.Fatalf("oops")
+				}
+			}(i, j)
+		}
+	}
+	wg.Wait()
+
+	os1, err = New(ctx, client, *testBucket, "prefix/")
+	if err != nil {
+		log.Fatalf("Failed to create object store: %v", err)
+	}
 
 	for i := 0; i < 6; i++ {
 		for j := 0; j < 2; j++ {
