fb46c8b08b056fba5a6046fecc2b1f32a39f0e8a
Nathan Bullock <nathanbullock@gmail.com>
2018-10-15 17:13:37 -0700

    More tests

--- integration_test.go
+++ integration_test.go
@@ -14,6 +14,32 @@ import (
 
 var testBucket = flag.String("bucket", "", "Bucket to use for integration tests")
 
+func GetKey(i, j int) string {
+	return fmt.Sprintf("key-%d-%d", i, j)
+}
+
+func GetData(i, j, gen int) string {
+	return []byte(fmt.Sprintf("data-%d-key-%d-%d", gen, i, j))
+}
+
+func GetDataFromKey(key string, gen int) string {
+	return []byte(fmt.Sprintf("data-%d-%s", gen, key))
+}
+
+func PerformXY(max_x, max_y int, funcxy func(x, y int)) {
+	var wg sync.WaitGroup
+	for x := 0; x < max_x; x++ {
+			for y := 0; y < max_y; y++ {
+		wg.Add(1)
+		go func(x_ int) {
+			defer wg.Done()
+				funcxy(x, y)
+		}(x)
+			}
+	}
+	wg.Wait()
+}
+
 func TestFullWorkCycle(t *testing.T) {
 	if *testBucket == "" {
 		t.Skip("Skipping integration test, this test uses a real GCS bucket.\n" +
@@ -33,51 +59,47 @@ func TestFullWorkCycle(t *testing.T) {
 		log.Fatalf("Failed to create object store: %v", err)
 	}
 
-	var wg sync.WaitGroup
-	for i := 0; i < 6; i++ {
-		wg.Add(1)
-		go func(z int) {
-			defer wg.Done()
-			for j := 0; j < 2; j++ {
-				log.Printf("Saving: Object:s%d%d", z, j)
-				os1.Insert(ctx, fmt.Sprintf("key%d%d", z, j),
-					[]byte(fmt.Sprintf("data%d%d", z, j)))
-				log.Printf("Saved: Object:s%d%d", z, j)
-			}
-		}(i)
-	}
-	wg.Wait()
+	gen := 1
+	log.Printf("Writing Gen1")
+	PerformXY(6, 2, func(x, y int) {
+				os1.Insert(ctx, GetKey(z, j), GetData(z, j, gen))
+})
 
-	for i := 0; i < 6; i++ {
-		for j := 0; j < 2; j++ {
-			wg.Add(1)
-			go func(x, y int) {
-				defer wg.Done()
-				g := os1.Get(fmt.Sprintf("key%d%d", x, y))
-				if bytes.Compare(g, []byte(fmt.Sprintf("data%d%d", x, y))) != 0 {
+	log.Printf("Validating Gen1")
+	PerformXY(6, 2, func(x, y int) {
+ func(x, y int) {
+				g := os1.Get(GetKey(x, y))
+				if bytes.Compare(g, GetData(x, y, gen)) != 0 {
 					log.Fatalf("oops")
 				}
-			}(i, j)
-		}
-	}
-	wg.Wait()
-
+ })
+		
+	log.Printf("Loading new ObjectStore")
 	os1, err = New(ctx, client, *testBucket, "prefix/")
 	if err != nil {
 		log.Fatalf("Failed to create object store: %v", err)
 	}
 
-	for i := 0; i < 6; i++ {
-		for j := 0; j < 2; j++ {
-			wg.Add(1)
-			go func(x, y int) {
-				defer wg.Done()
-				g := os1.Get(fmt.Sprintf("key%d%d", x, y))
-				if bytes.Compare(g, []byte(fmt.Sprintf("data%d%d", x, y))) != 0 {
+		log.Printf("Validating Gen1 (did persist and load work?)")
+	PerformXY(6, 2, func(x, y int) {
+ func(x, y int) {
+				g := os1.Get(GetKey(x, y))
+				if bytes.Compare(g, GetData(x, y))) != 0 {
 					log.Fatalf("oops")
 				}
-			}(i, j)
-		}
-	}
-	wg.Wait()
+
+	log.Printf("Writing Gen2")
+	gen = 2
+		go func(z int) {
+				os1.Insert(ctx, GetKey(z, j), GetData(z, j, gen))
+			}
+
+	 count := 0
+	 os1.ForEach(func(key string, value []byte) {
+		 count++;
+		 d := GetDataFromKey(key)
+				if bytes.Compare(value, d) != 0 {
+					log.Fatalf("oops")
+				}
+	 })
 }
